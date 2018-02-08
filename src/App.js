332d376fae2b8ae4a7d09604abc46522dd49b62e
Samuel Lee <evo0705@gmail.com>
2018-02-08 15:23:55 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Helmet } from "react-helmet"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Helmet>
+          <meta charSet="utf-8"/>
+          <title>This is Scrappable</title>
+          <meta name="description" content="This is to shows that React is Scrappable"/>
+          <meta name="keywords" content="react,scrappable"/>
+        </Helmet>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
